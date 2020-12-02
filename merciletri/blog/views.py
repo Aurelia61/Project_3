bd029011b036193ebcf5771d984678fed65bdaaf
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-12-02 17:59:39 +0100

    views + create other apps

--- merciletri/blog/views.py
+++ merciletri/blog/views.py
@@ -3,16 +3,35 @@ from django.views import generic
 
 from .models import Article
 
-def blog_index(request):
+
+class BlogIndex(generic.ListView):
     """
-        manage what it will be contain on the blog index
+        manage the blog index page thanks to django generic list view
     """
 
-    articles = Article.objects.all()
-    dict_articles = {'articles': articles}
+    # path to the template for this view
+    template_name = "blog/blog_index.html"
+
+    # which context is concerned by this view
+    context_object_name = 'latest_articles'
+
+    def get_queryset(self):
+        """
+            get and return the last 3 articles registred in the databases
+        """
+
+        #todo choisir le nombre d'articles à afficher sur la page d'index
 
 
-    return render(request, 'blog/blog_index.html', dict_articles)
+        articles_list_index = (Article.objects
+            #todo ranger par date et non par id
+            .order_by("-id")
+            # the 3 latest
+            [:3]
+        )
+
+        # Return the list of the 3 lastest article
+        return articles_list_index
 
 
 
@@ -22,7 +41,10 @@ class DetailArticle(generic.DetailView):
         manage the article page thanks to django generic detail view
     """
 
+    # what model is concerned by this view
     model = Article
+
+    # path to the template for this view
     template_name = "blog/article.html"
 
 
@@ -37,3 +59,15 @@ class DetailArticle(generic.DetailView):
 #     dict_article = {'article' : article}
 
 #     return render(request, 'blog/article.html', dict_article)
+
+
+# def blog_index(request):
+#     """
+#         manage what it will be contain on the blog index
+#     """
+
+#     articles = Article.objects.all()
+#     dict_articles = {'articles': articles}
+
+
+#     return render(request, 'blog/blog_index.html', dict_articles)

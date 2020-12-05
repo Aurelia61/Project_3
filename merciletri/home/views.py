8c393758cdad02861f6758a4513cbe927ecd53e4
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-12-05 15:47:25 +0100

    html in progress

--- merciletri/home/views.py
+++ merciletri/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """
+        #todo docstring
+    """
+
+    return render (request, 'home/main_layout.html')

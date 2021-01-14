25ee8fba2093da222a90369e1b3adc8d78bda3ca
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2021-01-14 19:21:07 +0100

    [ADD] game views & template [WIP] game.js

---
+++ merciletri/home/static/home/js/game.js
@@ -0,0 +1,33 @@
+console.log("It's working");
+    
+
+// $(document).ready(function () {
+//     let icon_moves = [["48%", "0%"], ["18%", "18%"], ["35%", "35%"], ["68%", "38%"],
+//     ["86%", "36%"], ["92%", "12%"], ["72%", "6%"]],
+//         icons = [],
+//         position = 0;
+//     for (let i = 1; i < 8; i++) {
+//         icons.push($("#moved-" + i));
+//     }
+
+//     function oneTurn() {
+//         for (let i = 0; i < icons.length; i++) {
+//             let currentElement = icons[i].attr("id").split("-")[1] - 1;
+//             currentElement += position;
+//             if (currentElement >= icon_moves.length) {
+//                 currentElement -= icon_moves.length;
+//             }
+//             icons[i].animate({
+//                 left: icon_moves[currentElement][0],
+//                 marginTop: icon_moves[currentElement][1],
+//             }, 1500, "swing");
+//         }
+//         position++;
+//         if (position === icons.length) {
+//             position = 0;
+//         }
+//     }
+//     setInterval(function () {
+//         oneTurn();
+//     }, 2500)
+// })

bed18befe32d6c00739f7673298d7a5f42ada1df
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2021-01-14 10:35:27 +0100

    [FIX] search on home page

--- merciletri/home/urls.py
+++ merciletri/home/urls.py
@@ -6,4 +6,7 @@ app_name = 'home'
 urlpatterns = [
     # the 'name' value as called by the {% url %} template tag
     path('', views.home, name = 'home'),
+    path('<int:city_id><str:city_name>/', views.home, name = 'home'),
+    path('<int:city_id><str:city_name>/<int:zone_id><str:zone_name>/', views.home, name = 'home'),
+
 ]

8f013d03d2a0d530640deb96b0915880aa61b330
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2021-01-24 10:04:06 +0100

    [FIX] game + [ADD] secret_key

--- merciletri/game/views.py
+++ merciletri/game/views.py
@@ -17,13 +17,20 @@ def game(request, garbage_id=None):
     image=None
     resultat=""
     selected_destination= None
-
-    good_destination=""
-    sel_dect="OUPS"
+    good_destination=None
+    sel_dest="OUPS"
 
     if request.method == "POST":
         ask_running = request.POST.get("ask_running", None)
         selected_destination = request.POST.get("selected_destination", None)
+
+        # get all the garbages
+        garbages = Garbage.objects.all().order_by('destination').distinct('destination')
+
+        # get all destinations without duplicates 
+        destinations = []
+        for dest in garbages:
+            destinations.append(dest.destination)
 
         if ask_running :
             running = True
@@ -33,29 +40,26 @@ def game(request, garbage_id=None):
             image = selected_garbage.image
             garbage_id = selected_garbage.id
 
-            # get all the garbages
-            garbages = Garbage.objects.all()
-
-            # get all destinations without duplicates 
-            for dest in garbages:
-                destinations.append(dest.destination)
-            destinations = list(dict.fromkeys(destinations))
-
         if selected_destination:
             dest_resultat = True
-            garbage = Garbage.objects.filter(id=garbage_id)
+            garbage = Garbage.objects.get(id=garbage_id)
+            if garbage.destination == destinations[int(selected_destination)]:
+                resultat = "C'est gagné !"
+            else:
+                good_destination = garbage.destination
+                resultat = "Perdu..."
 
-            for garb in garbage:
+            # for garb in garbage:
 
-                #test
-                sel_dect = (selected_destination).replace("a","X")
+            #     #test
+            #     sel_dest = (selected_destination).replace("a","X")
                 
-                if sel_dect == garb.destination:
-                    good_destination = garb.destination
-                    resultat = "C'est gagné !"
-                else:
-                    good_destination = garb.destination
-                    resultat = "Perdu"
+            #     if sel_dest == garb.destination:
+            #         good_destination = garb.destination
+            #         resultat = "C'est gagné !"
+            #     else:
+            #         good_destination = garb.destination
+            #         resultat = "Perdu"
 
     return render(
         request, 
@@ -70,8 +74,8 @@ def game(request, garbage_id=None):
             "Resultat" : resultat,
             "Running": running,
             "selected_destination":selected_destination,
-            "good_destination": good_destination,
-            "sel_dect":sel_dect,
+            "Good_destination": good_destination,
+            # "sel_dect":sel_dest,
             # "garbage": garbage,
             # "test_dest_garb": test_dest_garb
         })
--- merciletri/merciletri/settings.py
+++ merciletri/merciletri/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')9@2k-p#dhh6+$*s#kg+c2s18%udx8nc9c(#@jk*+9%@&mkpd7'
+# SECRET_KEY = ')9@2k-p#dhh6+$*s#kg+c2s18%udx8nc9c(#@jk*+9%@&mkpd7'
+SECRET_KEY = os.environ.get("SECRET_KEY", "mysecretkeypardefaut")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
